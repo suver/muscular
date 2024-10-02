2f5025ff988edcab572ba56e3871ffbfb9cf5c7b
Denis B <denis@butko.info>
2024-10-03 00:33:12 +0300

    fix version

--- src/muscles/core/__about__.py
+++ src/muscles/core/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-__name__ = "Muscular Framework"
-__version__ = "0.2.1.dev"
+__name__ = "Muscular Framework: core"
+__version__ = "0.0.3"
 __description__ = (
     "Muscular Framework"
 )
